d581dc62f26c1eecbd0536cecdf60752d3280c60
SergeyMoshkov <sergejmoshkov@gmail.com>
2023-03-12 21:10:37 +0300

    Настройка конфигурации в масштабах всего приложения

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,17 +2,30 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"stroy-service/pkg/config"
 	"stroy-service/pkg/handlers"
+	"stroy-service/pkg/render"
 )
 
 // main is the main function on our application
 func main() {
 
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+
 	http.FileServer(http.Dir("/static"))
 	http.HandleFunc("/about", handlers.About)
 	http.HandleFunc("/", handlers.Home)
 
+	fmt.Println("Starting application on port: 8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		fmt.Println(err)
 	}
